1c89080676823863e25095bde1042e243d05e7a1
aayangunna <88987151+aayangunna@users.noreply.github.com>
2022-08-09 07:57:07 +0100

    Add project files.

---
+++ JaggedArrayAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace JaggedArrayAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            int[][] jag = new int[][]
+            {
+                new int[] {1,3,7,5},
+                new int[] {7,5,3,4},
+                new int[] {2,3,9,5},
+                new int[] {1,3,9,5},
+            };
+
+            for (int i = 0; i < jag[i].Length; i++)
+            {
+                for (int j = 0; j < jag[i].Length; j++)
+                {
+                    Console.WriteLine(jag[i][j]);
+                }
+            }
+        }
+    }
+}
